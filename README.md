07eac3eed4cfaa6d352f0b98d0160627d2cdce7c
vertingo <admin@vertin-go.com>
2017-11-29 06:46:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Calcul d'intégral + plus représentation graphique
+
+Un programme SDL(Nécessite l'installation de SDL pour être compilé et exécuté voir ci-dessous) en langage c 
+
+
+## Prérequis
+Téléchargement et installation du logiciel Code::Blocks disponible en téléchargement sur le lien suivant: http://www.commentcamarche.net/download/telecharger-34056352-code-blocks
+
+1)Installation localement du compilateur MinGW disponible en téléchargement sur le lien suivant: https://sourceforge.net/projects/mingw-w64/
+
+2) Cliquez sur install puis continue
+3) Choisir mingw-developper-toolkit, mingw32-base, mingw32-gcc g++, msys-base comme sur
+la capture ci-dessous:
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-MinGW2.png" width="400" height="250"/></a>
+</p>
+4) Puis dans le menu « Installation » cliquez sur « Apply changes »
+5) Les packages sont alors téléchargés
+6) Quittez
+7) Exécutez « C:\MinGW\msys\1.0\postinstall », aux questions posées, répondre
+a. y (yes)
+b. y (yes)
+c. Donnez le chemin d’installation de mingw, c’est-à-dire à la racine du disque c: c:\mingw
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-MinGW.png" width="400" height="250"/></a>
+</p>
+d. validez avec la touche entrée
+
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-MinGW.png" width="400" height="250"/></a>
+</p>
+
+
+Installation de la bibliothèque SDL qui permet de créer des graphiques dans la console disponible sur le lien suivant: https://www.libsdl.org/
+Ensuite en bas à droite accéder à l'onglet download!
+Une fois téléchargé rendez-vous dans le dossier d'installation du logiciel Code::Blocks et suivez-les étapes illustrer ci-dessous!
+
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-sdl.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-sdl2.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/install-sdl3.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/newproject-sdl.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/newproject-sdl2.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/newproject-sdl3.png" width="400" height="250"/></a><br>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/newproject-sdl4.png" width="400" height="250"/></a><br>
+</p>
+
+
+## Usage
+Grâce à ce calculateur d'intégral vous allez pouvoir calculer l'intégral de n'importe quelle courbe mais également visualiser graphiquement l'air sous la courbe!
+Voyez les illustrations ci-dessous!
+
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/image1.png" width="400" height="250"/></a>
+</p>
+
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/image_courbe.png" width="400" height="250"/></a>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/image_courbe2.png" width="400" height="250"/></a>
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="images/image_courbe3.png" width="400" height="250"/></a>
+</p>
+
+
+## Details
+
+```
+Un petit coup de boost suivez-nous sur YouTube et Facebook!
+[You Tube]: https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1
+[Facebook]: https://www.facebook.com/vertingo/
+```
+
+<p align="center">
+  <a href="https://www.youtube.com/channel/UC2g_-ipVjit6ZlACPWG4JvA?sub_confirmation=1"><img src="http://vertin-go.com/Fonctions_Annexes/annexes/pdt-page-de-telechargement/Android%20You%20Tube%20Data%20API/youtube2.png" width="400" height="250"/></a>
+  <a href="https://www.facebook.com/vertingo/"><img src="http://vertin-go.com/Fonctions_Annexes/annexes/pdt-page-de-telechargement/Android%20You%20Tube%20Data%20API/rejoins_nous.png" width="400" height="250"/></a>
+</p>
---
+++ main.c
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
+#include <sdl/sdl.h>
+
+#define XRES 800
+#define YRES 600
+#define MINX 0.0
+#define MINY -6.0
+#define MAXX 25.0
+#define MAXY 630.0
+
+#ifdef WIN32
+#pragma comment(lib,"sdl.lib")
+#pragma comment(lib,"sdlmain.lib")
+#endif
+
+double f(double x)
+{
+    // changez ici la fonction que vous voulez afficher. (pas forcément un polynome)
+    return x*x + 5;
+}
+
+void waitkey()            // attend qu'on appuie sur ESC
+{
+    SDL_Event event;
+    while(1)            // boucle
+    {
+        while(SDL_PollEvent(&event))        // aquisition d'evenement
+        {
+            if (event.type == SDL_KEYDOWN)  // on appuie sur une touche ?
+            {
+                if (event.key.keysym.sym == SDLK_ESCAPE) return;  // c'est "ESC" ?
+            }
+        }
+        SDL_Delay(1);
+    }
+}
+
+void SDL_PutPixel32(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+    Uint8 *p;
+    if (x<0 || y<0 || x>surface->w-1 || y>surface->h-1)
+        return;
+    p = (Uint8*)surface->pixels + y * surface->pitch + x * 4;
+    *(Uint32*)p = pixel;
+}
+
+void Line(SDL_Surface* surf,int x1,int y1, int x2,int y2,Uint32 couleur)  // Bresenham
+{
+    int x,y;
+    int Dx,Dy;
+    int xincr,yincr;
+    int erreur;
+    int i;
+
+    Dx = abs(x2-x1);
+    Dy = abs(y2-y1);
+    if(x1<x2)
+        xincr = 1;
+    else
+        xincr = -1;
+    if(y1<y2)
+        yincr = 1;
+    else
+        yincr = -1;
+
+    x = x1;
+    y = y1;
+    if(Dx>Dy)
+    {
+        erreur = Dx/2;
+        for(i=0;i<Dx;i++)
+        {
+            x += xincr;
+            erreur += Dy;
+            if(erreur>Dx)
+            {
+                erreur -= Dx;
+                y += yincr;
+            }
+            SDL_PutPixel32(surf,x, y,couleur);
+        }
+    }
+    else
+    {
+        erreur = Dy/2;
+        for(i=0;i<Dy;i++)
+        {
+            y += yincr;
+            erreur += Dx;
+            if(erreur>Dy)
+            {
+                erreur -= Dy;
+                x += xincr;
+            }
+            SDL_PutPixel32(surf,x, y,couleur);
+        }
+    }
+    SDL_PutPixel32(surf,x1,y1,couleur);
+    SDL_PutPixel32(surf,x2,y2,couleur);
+}
+
+int Re_to_EcrX(double r,double minX,double maxX)
+{
+    return (int)(XRES*(r-minX)/(maxX-minX));
+}
+
+int Re_to_EcrY(double r,double minY,double maxY)
+{
+    int y = (int)(YRES*(r-minY)/(maxY-minY));
+    y = YRES - y - 1;
+    return y;
+}
+
+double Ecr_to_ReX(int i,double minX,double maxX)
+{
+    return minX + i*(maxX-minX)/XRES;
+}
+
+double Ecr_to_ReY(int i,double minY,double maxY)
+{
+    return maxY - i*(maxY-minY)/YRES;
+}
+
+
+void ShowFoncion(SDL_Surface* screen,Uint32 couleur,double (*fonc)(double),int minX,int maxX,int minY,int maxY)
+{
+    int i;
+    int lastx = 0;
+    int lasty = 0;
+    int x,y;
+    double resfonc;
+
+    SDL_Surface *rectangle = SDL_CreateRGBSurface(0,5,5,32,0,0,0,0);
+    SDL_Rect surface_rect = {4,1,5,5};
+
+    for(i=0;i<XRES;i++)
+    {
+        x = i;
+        resfonc = fonc(Ecr_to_ReX(i,minX,maxX));
+        y = Re_to_EcrY(resfonc,minY,maxY);
+
+       /* SDL_FillRect(rectangle, &surface_rect, SDL_MapRGB(rectangle->format,255,0,0));
+        SDL_BlitSurface(rectangle, NULL, screen, &surface_rect);*/
+
+        if (i!=0)
+            Line(screen,x,y,lastx,lasty,couleur);
+        lastx = x;
+        lasty = y;
+
+    }
+}
+
+void ShowAxis(SDL_Surface* screen,int minX,int maxX,int minY,int maxY)
+{
+    int centreX = Re_to_EcrX(0.0,minX,maxX);
+    int centreY = Re_to_EcrY(0.0,minY,maxY);
+    Line(screen,centreX,0,centreX,YRES-1,0xFFFFFF);  // axe vertical
+    Line(screen,0,centreY,XRES-1,centreY,0xFFFFFF);  // axe horizontal
+    Uint32 rouge = SDL_MapRGB(screen->format, 255, 0, 0);
+    Uint32 bleu = SDL_MapRGB(screen->format, 0, 0, 255);
+    Line(screen,(XRES/(maxX+1))*maxX,0,(XRES/(maxX+1))*maxX,YRES-1,rouge);
+    Line(screen,(XRES/(maxX+1)),0,(XRES/(maxX+1)),YRES-1,rouge);
+
+    int xmax,xpas,y;
+    double resfonc2;
+    xmax=(XRES/(maxX+1))*maxX;
+    xpas=(XRES/(maxX+1));
+
+    for(int i=xpas;i<xmax;i++)
+    {
+        resfonc2 = f(Ecr_to_ReX(i,minX,maxX));
+        y = Re_to_EcrY(resfonc2,minY,maxY);
+        Line(screen,i,y,i,centreY,bleu);
+    }
+
+    while(0);
+}
+
+/*Function definition to perform integration by Trapezoidal Rule */
+double trapezoidal(double a,double b,int n){
+  double x,h=0,sum=0,integral;
+  double i;
+  h=fabs(b-a)/n;
+
+  for(i=1;i<n;i++){
+    x=a+i*h;
+    sum=sum+f(x);
+  }
+  integral=(h/2)*(f(a)+f(b)+2*sum);
+
+  return integral;
+}
+
+double simpson(int n, double a, double b)
+{
+  double X, h, Iapp0, Iapp1, Iapp2, Iapp;
+  int NN, i;
+
+  // Etape 1
+  h = (b - a) / n;
+
+  // Etape 2
+  Iapp0 = f(a) + f(b);
+  Iapp1 = 0.0;
+  Iapp2 = 0.0;
+
+  // Etape 3
+  NN = n -1;
+  for (i=1; i<=NN; i++)
+    {
+      // Etape 4
+      X = a + i*h;
+      // Etape 5
+      if ((i%2) == 0)
+        Iapp2 = Iapp2 + f(X);
+      else
+        Iapp1 = Iapp1 + f(X);
+    }
+
+  // Etape 6
+  Iapp = (Iapp0 + 2.0 * Iapp2 + 4.0 * Iapp1) * h / 3.0;
+
+  // Etape 7
+  return (Iapp);
+
+}
+
+
+int main(int argc, char *argv[])
+{
+  int i,n;
+  float a,b,spmthd;
+  double minX, maxX, minY, maxY;
+  char fonction[10];
+  int fonction2[15];
+  int choix;
+  SDL_Surface *screen;
+
+  a=0; /* Borne inferieure */
+  b=0; /* Borne superieure */
+  n=0; /* Nombre d'iteration  */
+
+  SDL_Init(SDL_INIT_VIDEO);
+  freopen("CON", "w", stdout); // redirects stdout
+  freopen("CON", "w", stderr); // redirects stderr
+
+  printf("Entrez la valeur a de la borne inferieure de l'interval de l'integral!\na=");
+  scanf("%f",&a);
+  printf("Entrez la valeur b de la borne superieure de l'interval de l'integral!\nb=");
+  scanf("%f",&b);
+  printf("Entrez la valeur n correspondant au nombre de decoupage de l'interval en trapeze!\nn=");
+  scanf("%i",&n);
+  trapezoidal(a,b,n);
+  spmthd=trapezoidal(a,b,n);
+  printf("\nValeur de l'integral: %f \n",spmthd);
+
+  printf("\nEntrez le numero de l\'operation desiree?:\n");
+  printf("\n(1) Afficher une representation graphique de la courbe?\n",spmthd);
+  printf("\nQuel est votre choix? :");
+
+  scanf("%d",&choix);
+
+  switch(choix)
+  {
+   case 1:
+         {
+           minX=a-1;
+           maxX=b+1;
+           minY=-50;
+           maxY=f(b)+1;
+
+           screen=SDL_SetVideoMode(XRES,YRES,32,SDL_SWSURFACE|SDL_DOUBLEBUF);
+
+           if(SDL_MUSTLOCK(screen))
+           SDL_LockSurface(screen);
+
+           ShowAxis(screen,minX,maxX,minY,maxY);
+
+           ShowFoncion(screen,0x00FF00,f,minX,maxX,minY,maxY);
+
+           if(SDL_MUSTLOCK(screen))
+           SDL_UnlockSurface(screen);
+           SDL_Flip(screen);
+           waitkey();
+           return 0;
+
+          };break;
+
+  }
+
+}
